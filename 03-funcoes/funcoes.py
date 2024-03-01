b665f3d094f3f751390a51150253b0f9e6da2639
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-03-01 10:09:18 -0300

    .\03-funcoes\

---
+++ 03-funcoes/funcoes.py
@@ -0,0 +1,11 @@
+# funções
+
+# declarando funçoes
+def cabecalho(p1): #p1 parametro
+    print(p1 * 30)
+    print("RELATÓRIO")
+    print(p1 * 30)
+
+# chamar função
+cabecalho("-") #define parametro dentro do parenteses
+
