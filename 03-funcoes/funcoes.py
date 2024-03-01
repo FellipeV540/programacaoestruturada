b5115919e1989f8f19fe69eb5807e3af978e0508
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-03-01 11:01:58 -0300

    .\03-funcoes\

--- 03-funcoes/funcoes.py
+++ 03-funcoes/funcoes.py
@@ -1,30 +1,86 @@
-# funções
+"""
+Programação Estruturada
+2024.1
+01/03/2024
+
+Funções
+- Encapsular uma informação
+    - Isolar o comportamento para facilitar o uso
+- Organizar o código
+"""
+
+CAMINHO_DO_PROJETO = "C:\\projetos"
 
 def traco(sep, tamanho):
     print(sep * tamanho)
 
-# declarando/definindo funçoes
-def cabecalho(titulo, sep= "-", tamanho= 30): #p1 parametro
-    print(sep * tamanho)
+# Declaração / definição de função
+def cabecalho(titulo, sep="-", tamanho=30):
+    traco(sep, tamanho)
     print(titulo)
-    print(sep * tamanho)
+    traco(sep, tamanho)
 
-# chamar função
-cabecalho("RELATORIO DE FINANÇAS") #define parametro dentro do parenteses
-cabecalho("RELATORIO DE CONTRATAÇÕES", 25, "=") #define parametro dentro do parenteses
+# Chamando uma função
+cabecalho("Relatório de despesas")
+cabecalho("Folha de pagamento", tamanho=25, sep=".")
 
-# nao colocoar os parenteses significa que estou falando da funçao, e nao chamando
+# Não colocar os parênteses significa que estou
+# falando da função, e não chamando
 print(cabecalho)
-print(cabecalho("ola", 25))
+print(cabecalho("olá", tamanho=25))
 
-print("-" * 40)
+# Toda função tem um retorno
+print(print("olá, mundo"))
+# print(input("nome: "))
 
-x = 777
+def soma(a, b):
+    return a + b
+
+print(soma(2, 5) + soma(4, 9))
+
+x = 2
+print(x % 2 == 0)
+
+def e_par(num):
+    return num % 2 == 0
+
+print(e_par(8))
+print(e_par(55))
+
+def troca_valores(a, b):
+    return b, a # esse retorno é uma tupla
+
+print(troca_valores(8, 0))
+y = 5
+
+x, y = troca_valores(x, y)
+print(x, y)
+
+# Escopo de variáveis
+def func1():
+    a = 10
+    print(a) # escopo local
+    print(x) # escopo global
+
+print("-" * 30)
+func1()
 
 def func2(x):
     x = 999
-    print(x)
-    return x
+    print(x) # escopo local tem preferência
 
-x = func2(100)
+func2(100)
 print(x)
+
+def func3():
+    global x  # NÃO USAR!!!!!!
+    x = 0
+    print(x)
+
+func3()
+print(x) # 0
+
+def func4():
+    print(CAMINHO_DO_PROJETO)
+
+func4()
---
+++ 03-funcoes/projeto.py
@@ -0,0 +1,21 @@
+# aplicação de funções
+
+def soma(a, b):
+    return a + b
+
+def outra_soma():
+    a = float(input("Primeiro Numero: "))
+    b = float)input("Segundo Numero: ")
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def main():
+    print(soma(5, 8))
+    print(subtracao(5, 8))
+    print(outra_soma())
+    print()
+
+
+main()
