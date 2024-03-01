1ac247502cf397110980d15437cf063bc1ef4365
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-03-01 08:23:40 -0300

    .\02-operacoes_python\

---
+++ 02-operacoes_python/02-operacoes.py
@@ -0,0 +1,24 @@
+"""
+operações em python
+- Aritimeticas
+-Atribuição
+- Comparação (ou Relacionais)
+- Logicas (ou Booleanas)
+"""
+
+# Operações Aritimeticas
+# Dois operandos numéricos
+x = 8.4
+y = 2.3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)  # divisao normal
+print(x ** y) # potencia
+print(x // y) # divisao (inteira)
+print(x % y)  # modulo (sobra da divisao)
+
+print(round(x - y,2))
+
+# um operando é uma str
