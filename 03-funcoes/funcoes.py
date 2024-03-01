647167357f4835e27849af928919a8db92415bff
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-03-01 10:42:57 -0300

    .\03-funcoes\

--- 03-funcoes/funcoes.py
+++ 03-funcoes/funcoes.py
@@ -1,11 +1,20 @@
 # funções
 
-# declarando funçoes
-def cabecalho(p1): #p1 parametro
-    print(p1 * 30)
-    print("RELATÓRIO")
-    print(p1 * 30)
+def traco(sep, tamanho)
+    print(sep * tamanho)
+
+# declarando/definindo funçoes
+def cabecalho(titulo, sep= "-", tamanho= 30): #p1 parametro
+    print(sep * tamanho)
+    print(titulo)
+    print(sep * tamanho)
 
 # chamar função
-cabecalho("-") #define parametro dentro do parenteses
+cabecalho("RELATORIO DE FINANÇAS") #define parametro dentro do parenteses
+cabecalho("RELATORIO DE CONTRATAÇÕES", 25, "=") #define parametro dentro do parenteses
+
+# nao colocoar os parenteses significa que estou falando da funçao, e nao chamando
+print(cabecalho)
+print(cabecalho("ola", 25))
+
 
