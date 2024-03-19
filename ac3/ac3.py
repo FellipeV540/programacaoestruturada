5586773bbd7867f5599e8664252f6a9da97a1ddf
Fellipe Valladares <fellipevalladares@gmail.com>
2024-03-19 15:36:07 -0300

    ac3 concluida

--- ac3/ac3.py
+++ ac3/ac3.py
@@ -16,3 +16,48 @@ def testa_triangulo():
     print(determina_tipo_triangulo(1, 1, 4)) # Não é um triângulo
 
 testa_triangulo()
+
+def dia_semana(dia):
+    if 0 < dia < 8:
+        if dia == 1:
+            return "Domingo"
+        elif dia == 2:
+            return "Segunda-Feira"
+        elif dia == 3:
+            return "Terça-Feira"
+        elif dia == 4:
+            return "Quarta-Feira"
+        elif dia == 5:
+            return "Quinta-Feira"
+        elif dia == 6:
+            return "Sexta-Feira"
+        elif dia == 7:
+            return "Sábado"
+    else:
+        return "String Vazia"
+
+def testa_dia_semana():
+    print(dia_semana(2)) # segunda-feira
+    print(dia_semana(6)) # sexta-feira
+    print(dia_semana(7)) # sábado
+    print(dia_semana(9)) # string vazia
+
+testa_dia_semana()
+
+def calc():
+    a = float(input("Primeiro Número: "))
+    b = float(input("Segundo Número: "))
+    op = (str(input("Operação Desejada: ")))
+
+    if op.lower() == "soma":
+        print(a + b)
+    elif op.lower() == "subtracao":
+        print(a - b)
+    elif op.lower() == "multiplicacao":
+        print(a * b)
+    elif op.lower() == "divisao":
+        print(a / b)
+    else:
+        print("Operação Inválida")
+
+calc()
